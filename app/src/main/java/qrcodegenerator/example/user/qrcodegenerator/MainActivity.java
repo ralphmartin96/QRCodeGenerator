ec0af0137200ace3a168ee297dcb9c3ad82e2878
ralphmartin96 <ralph.martin.montero@gmail.com>
2016-06-29 09:35:17 +0800

    Initial commit with QR Code Generation

---
+++ app/src/main/java/qrcodegenerator/example/user/qrcodegenerator/MainActivity.java
@@ -0,0 +1,52 @@
+package qrcodegenerator.example.user.qrcodegenerator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    //DatabaseHelper myDb;
+
+    Button generateBtn;
+    EditText input;
+
+    public final static int WIDTH=500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        generateBtn = (Button) findViewById(R.id.generateBtn);
+        input = (EditText) findViewById(R.id.input);
+
+        generateBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+                //Toast.makeText(MainActivity.this, input.getText(), Toast.LENGTH_SHORT).show();
+
+                Intent intent = new Intent(MainActivity.this, QRGenerator.class);
+                intent.putExtra("input", input.getText().toString());
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/qrcodegenerator/example/user/qrcodegenerator/QRGenerator.java
@@ -0,0 +1,110 @@
+package qrcodegenerator.example.user.qrcodegenerator;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+
+/**
+ * Created by user on 6/24/16.
+ */
+public class QRGenerator extends AppCompatActivity {
+
+    ImageView qrCodeImageview;
+    String QRcode;
+    public final static int WIDTH=500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_output);
+
+        Intent intent = getIntent();
+
+        QRcode = intent.getStringExtra("input");
+
+        Toast.makeText(QRGenerator.this, QRcode , Toast.LENGTH_SHORT).show();
+        getID();
+        runThread();
+    }
+
+    private void runThread(){
+
+        Thread t = new Thread(new Runnable() {
+
+            public void run() {
+
+                try {
+
+                    synchronized (this) {
+                        wait(5000);
+                        // runOnUiThread method used to do UI task in main thread.
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                try {
+                                    Bitmap bitmap = null;
+
+                                    bitmap = encodeAsBitmap(QRcode);
+                                    qrCodeImageview.setImageBitmap(bitmap);
+
+                                } catch (WriterException e) {
+                                    e.printStackTrace();
+                                } // end of catch block
+
+                            } // end of run method
+
+                        });
+
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        });
+
+        t.start();
+
+    }
+
+    private void getID() {
+        qrCodeImageview=(ImageView) findViewById(R.id.img_qr_code_image);
+    }
+
+    // this is method call from on create and return bitmap image of QRCode.
+    Bitmap encodeAsBitmap(String str) throws WriterException {
+        BitMatrix result;
+        try {
+            result = new MultiFormatWriter().encode(str,
+                    BarcodeFormat.QR_CODE, WIDTH, WIDTH, null);
+        } catch (IllegalArgumentException iae) {
+            // Unsupported format
+            return null;
+        }
+        int w = result.getWidth();
+        int h = result.getHeight();
+        int[] pixels = new int[w * h];
+        for (int y = 0; y < h; y++) {
+            int offset = y * w;
+            for (int x = 0; x < w; x++) {
+                pixels[offset + x] = result.get(x, y) ? getResources().getColor(R.color.black):getResources().getColor(R.color.white);
+            }
+        }
+        Bitmap bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        bitmap.setPixels(pixels, 0, 500, 0, 0, w, h);
+        return bitmap;
+    } /// end of this method*/
+
+}
